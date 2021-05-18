237d9f331af02b43c9540bab2d35486b5e342eae
Maksim Ustinov <mustinov@keepersecurity.com>
2021-05-18 14:45:40 -0700

    e

--- entrypoint.py
+++ entrypoint.py
@@ -11,16 +11,20 @@ for k, v in os.environ.items():
 # os.environ['INPUT_NAME'] = 'Actions Toolkit'
 # who_to_greet = core.get_input('who-to-greet')
 
-# core.info('who_to_greet=%s' % who_to_greet)
 
-core.error('TEST ERROR: Something went wrong.')
+# core.error('TEST ERROR: Something went wrong.')
 
 
 core.info('TEST INFO: Run successfully.')
 
-KEEPER_SECRET_KEY = environ.get('KEEPER_SECRET_KEY') 
+SECRET_CONFIG = environ.get('SECRET_CONFIG') 
+SECRETS = environ.get('SECRETS')
+
+
+
 # KEEPER_SECRET_KEY = core.get_input('keeper-secret-key')
-core.info('KEEPER_SECRET_KEY=%s' % KEEPER_SECRET_KEY)
+core.info('SECRET_CONFIG=%s' % SECRET_CONFIG)
+core.info('SECRETS=%s' % SECRETS)
 
 
 
