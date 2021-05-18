864e955e1e45f9cb721a70233307ef9800e2b50e
Maksim Ustinov <mustinov@keepersecurity.com>
2021-05-18 10:14:00 -0700

    d

--- entrypoint.py
+++ entrypoint.py
@@ -1,5 +1,9 @@
 import os
+from os import environ
 from actions_toolkit import core
+
+from keepercommandersm import Commander
+from keepercommandersm.storage import FileKeyValueStorage
 
 for k, v in os.environ.items():
     print(f'{k}={v}')
@@ -14,6 +18,10 @@ core.error('TEST ERROR: Something went wrong.')
 
 
 core.info('TEST INFO: Run successfully.')
+
+KEEPER_SECRET_KEY = environ.get('KEEPER_SECRET_KEY') 
+core.info('KEEPER_SECRET_KEY=%s' % KEEPER_SECRET_KEY)
+
 
 
 # core.set_failed('TEST ERROR: SSL certificates installation failed.')
