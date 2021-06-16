c3fc80749f3baf6b3fe1a30c3362df50f6523762
Maksim Ustinov <mustinov@keepersecurity.com>
2021-06-16 13:39:42 -0700

    Capability to add different fields and custom fields values

--- ghaentrypoint.py
+++ ghaentrypoint.py
@@ -19,7 +19,8 @@ class RecordActionEntry:
 
     def __init__(self):
         self.uid = None
-        self.field = None
+        self.field_type = None
+        self.field_value = None
         self.destination_type = DestinationKey.ENV
         self.destination_val = None
 
@@ -36,31 +37,43 @@ class RecordActionEntry:
     @staticmethod
     def from_entry(record_action_entry_str):
 
-        se_parts = record_action_entry_str.split('|')       # [uid123 password], [PASSWORD]
+        se_parts = record_action_entry_str.split('|')       # [uid123 field:password], [PASSWORD]
 
         record_details_str = se_parts[0].strip()            # uid123 password
-        record_details_arr = record_details_str.split()     # ['uid123', 'password'] OR ['uid321', 'file:config.json']
+        record_details_arr = record_details_str.split()     # ['uid123', 'field:password'] OR ['uid321', 'file:config.json']
         record_uid = record_details_arr[0]                  # 'uid123'
-        secret_value_location = record_details_arr[1]       # Field to retrieve. ex. 'password' OR 'file:config.json'
-
-        destination_str = se_parts[1].strip()
-
-        destination_arr = destination_str.split(':')
-
-        if len(destination_arr) == 1:
-            destination_key = DestinationKey.ENV
-            destination_val = destination_arr[0]
-        elif len(destination_arr) == 2:
-            destination_key = DestinationKey(destination_arr[0])
-            destination_val = destination_arr[1]
-        else:
-            raise Exception("Destination string was not properly formatted. Err #DE1")
+        secret_value_location = record_details_arr[1]       # Field to retrieve. ex.
+                                                                                # 'password' OR
+                                                                                # 'field:login' OR
+                                                                                # 'custom:MyField' OR
+                                                                                # 'file:config.json'
 
         rae = RecordActionEntry()
         rae.uid = record_uid.strip()
-        rae.field = secret_value_location.strip()
-        rae.destination_type = destination_key
-        rae.destination_val = destination_val.strip()
+
+        # 1. Source
+        secret_value_location_arr = secret_value_location.split(':')
+        if len(secret_value_location_arr) == 1:
+            rae.field_type = 'field'
+            rae.field_value = secret_value_location_arr[0]
+        elif len(secret_value_location_arr) == 2:
+            rae.field_type = secret_value_location_arr[0]
+            rae.field_value = secret_value_location_arr[1]
+        else:
+            raise Exception("Source string was not properly formatted. Err #DE1")
+
+        # 2. Destination
+        destination_str = se_parts[1].strip()
+        destination_arr = destination_str.split(':')
+
+        if len(destination_arr) == 1:
+            rae.destination_type = DestinationKey.ENV
+            rae.destination_val = destination_arr[0]
+        elif len(destination_arr) == 2:
+            rae.destination_type = DestinationKey(destination_arr[0])
+            rae.destination_val = destination_arr[1]
+        else:
+            raise Exception("Destination string was not properly formatted. Err #DE2")
 
         return rae
 
@@ -76,8 +89,7 @@ def find_record(secrets, search_term):
 
 
 def __save_to_file(record, rae):
-    file_name = rae.field                   # ex. 'file:config.json'
-    file_name = file_name.split(':')[1]     # ex. 'config.json'
+    file_name = rae.field_value                  # ex. 'config.json'
 
     core.info("Processing file %s" % file_name)
     core.debug("Number of files in secret: %s" % len(record.files))
@@ -153,6 +165,7 @@ def run_action():
 
     count = 0
     outputs_map = {}
+    env_map = {}
 
     for record_action in record_actions:
 
@@ -162,26 +175,28 @@ def run_action():
 
         record = find_record(retrieved_secrets, record_action.uid)
 
-        core.set_secret(record.password)
-
         if not record:
             core.warning("Record uid=%s not found. Make sure you have this record added to the application you are using." % record_action.uid)
         else:
-            core.info("Secret uid=%s, dest=%s" % (record.uid, record_action.destination_type))
-
-            if record_action.destination_type != DestinationKey.FILE and record_action.field != 'password':
-                raise Exception("Currently supporting only password fields or files")
+            core.info("Secret uid=%s, source field type=[%s], source field value=[%s], dest=%s" % (
+                record.uid,
+                record_action.field_type,
+                record_action.field_value,
+                record_action.destination_type))
 
             if record_action.destination_type == DestinationKey.ENV:
-                if record.password:
 
-                    github_env = os.environ.get('GITHUB_ENV')
-
-                    with open(github_env, 'a') as github_env_file:
-                        github_env_file.write("%s=%s" % (record_action.destination_val, record.password))
-
+                if record_action.field_value == 'title':
+                    secret_value = record.title
+                elif record_action.field_type == 'field':
+                    secret_value = record.field(record_action.field_value)[0]
+                elif record_action.field_type == 'custom':
+                    secret_value = record.custom_field(record_action.field_value)[0]
                 else:
-                    core.warning("Password field is empty")
+                    raise Exception("Currently supporting only fields and custom fields in the record")
+
+                env_map[record_action.destination_val] = secret_value
+                core.set_secret(secret_value)   # hiding any values, even title
 
             elif record_action.destination_type == DestinationKey.OUT:
                 outputs_map[record_action.destination_val] = record.password
@@ -195,8 +210,21 @@ def run_action():
         core.debug('out-secrets = %s' % outputs_json)
         core.set_output('out-secrets', outputs_json)
 
-    os.environ['MyTest'] = 'This Is Max'
+    write_to_env(env_map)
+
     core.info("Finish retrieving secrets from Keeper Security")
+
+
+def write_to_env(env_map):
+    if env_map:
+        github_env_file = os.environ.get('GITHUB_ENV')
+
+        env_map_str = ''
+        for key, value in env_map.items():
+            env_map_str += '%s=%s\n' % (key, value)
+
+        with open(github_env_file, 'a') as github_env_file:
+            github_env_file.write(env_map_str)
 
 
 if __name__ == '__main__':
